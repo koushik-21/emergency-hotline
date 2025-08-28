a934cfee166da7c88db0fdd64dede60d40f6d72d
koushik-21 <koushik.biswas.edu@gmail.com>
2025-08-28 18:11:40 +0600

    pc-updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 /////////////////////////////////// [card-section] ///////////////////////////////////////////
-/////////////////////////////////// [national emergency number - 999]
+/////////////////////////////////// [National Emergency Number]
 document.getElementById('call-national-emergency').addEventListener('click', function (event) {
     console.log('hello, Is this the 999 ??');
     const serviceName = document.getElementById('national-emergency').innerText;
     const serviceNum = document.getElementById('national-emergency-number').innerText;
     let time = new Date().toLocaleTimeString('en-US', { hour12: true });
-    //  alert('Service name: '+serviceName +'  Phone-number: '+serviceNum);
+    alert('Calling ' + serviceName + ' ' + serviceNum + '...');
     const coin = document.getElementById('coin').innerText;
     if (coin >= 20) {
         const newCoin = coin - 20;
@@ -36,7 +36,7 @@ document.getElementById('call-police-number').addEventListener('click', function
     const serviceName = document.getElementById('police-help').innerText;
     const serviceNum = document.getElementById('police-help-number').innerText;
     let time = new Date().toLocaleTimeString('en-US', { hour12: true });
-    //  alert('Service name: '+serviceName +'  Phone-number: '+serviceNum);
+    alert('Calling ' + serviceName + ' ' + serviceNum + '...');
     const coin = document.getElementById('coin').innerText;
     if (coin >= 20) {
         const newCoin = coin - 20;
@@ -67,7 +67,7 @@ document.getElementById('call-fire-service').addEventListener('click', function 
     const serviceName = document.getElementById('fire-service').innerText;
     const serviceNum = document.getElementById('fire-service-number').innerText;
     let time = new Date().toLocaleTimeString('en-US', { hour12: true });
-    //  alert('Service name: '+serviceName +'  Phone-number: '+serviceNum);
+    alert('Calling ' + serviceName + ' ' + serviceNum + '...');
     const coin = document.getElementById('coin').innerText;
     if (coin >= 20) {
         const newCoin = coin - 20;
@@ -98,7 +98,7 @@ document.getElementById('call-ambulance-service').addEventListener('click', func
     const serviceName = document.getElementById('ambulance-service').innerText;
     const serviceNum = document.getElementById('ambulance-service-number').innerText;
     let time = new Date().toLocaleTimeString('en-US', { hour12: true });
-    //  alert('Service name: '+serviceName +'  Phone-number: '+serviceNum);
+    alert('Calling ' + serviceName + ' ' + serviceNum + '...');
     const coin = document.getElementById('coin').innerText;
     if (coin >= 20) {
         const newCoin = coin - 20;
@@ -129,7 +129,7 @@ document.getElementById('call-wAndC-helpline').addEventListener('click', functio
     const serviceName = document.getElementById('wAndC-helpline').innerText;
     const serviceNum = document.getElementById('wAndC-helpline-number').innerText;
     let time = new Date().toLocaleTimeString('en-US', { hour12: true });
-    //  alert('Service name: '+serviceName +'  Phone-number: '+serviceNum);
+    alert('Calling ' + serviceName + ' ' + serviceNum + '...');
     const coin = document.getElementById('coin').innerText;
     if (coin >= 20) {
         const newCoin = coin - 20;
@@ -160,7 +160,7 @@ document.getElementById('call-anti-corruption').addEventListener('click', functi
     const serviceName = document.getElementById('anti-corruption').innerText;
     const serviceNum = document.getElementById('anti-corruption-number').innerText;
     let time = new Date().toLocaleTimeString('en-US', { hour12: true });
-    //  alert('Service name: '+serviceName +'  Phone-number: '+serviceNum);
+    alert('Calling '+serviceName + ' ' +serviceNum+'...');
     const coin = document.getElementById('coin').innerText;
     if (coin >= 20) {
         const newCoin = coin - 20;
@@ -191,7 +191,7 @@ document.getElementById('call-electricity-helpline').addEventListener('click', f
     const serviceName = document.getElementById('electricity-helpline').innerText;
     const serviceNum = document.getElementById('electricity-helpline-number').innerText;
     let time = new Date().toLocaleTimeString('en-US', { hour12: true });
-    //  alert('Service name: '+serviceName +'  Phone-number: '+serviceNum);
+    alert('Calling '+serviceName + ' ' +serviceNum+'...');
     const coin = document.getElementById('coin').innerText;
     if (coin >= 20) {
         const newCoin = coin - 20;
@@ -222,7 +222,7 @@ document.getElementById('call-brac-helpline').addEventListener('click', function
     const serviceName = document.getElementById('brac-helpline').innerText;
     const serviceNum = document.getElementById('brac-helpline-number').innerText;
     let time = new Date().toLocaleTimeString('en-US', { hour12: true });
-    //  alert('Service name: '+serviceName +'  Phone-number: '+serviceNum);
+    alert('Calling '+serviceName + ' ' +serviceNum+'...');
     const coin = document.getElementById('coin').innerText;
     if (coin >= 20) {
         const newCoin = coin - 20;
@@ -253,7 +253,7 @@ document.getElementById('call-bdRailway-helpline').addEventListener('click', fun
     const serviceName = document.getElementById('bdRailway-helpline').innerText;
     const serviceNum = document.getElementById('bdRailway-helpline-number').innerText;
     let time = new Date().toLocaleTimeString('en-US', { hour12: true });
-    //  alert('Service name: '+serviceName +'  Phone-number: '+serviceNum);
+    alert('Calling '+serviceName + ' ' +serviceNum+'...');
     const coin = document.getElementById('coin').innerText;
     if (coin >= 20) {
         const newCoin = coin - 20;
@@ -280,10 +280,75 @@ document.getElementById('call-bdRailway-helpline').addEventListener('click', fun
 })
 
 
-
 ///////////////////////////////// [Call History Section] ////////////////////////////
 //////////////////// [clear the call records]
 document.getElementById('clear-history').addEventListener('click', function (event) {
     const parentNode = document.getElementById('call-history');
     parentNode.innerHTML = ``;
 })
+
+//////////////////////////////////////////Copy Button Work //////////////////////////
+//////////////////// [Copy button Counter]
+document.body.addEventListener('click', e => {
+    if (e.target.classList.contains('cpy-btn')) {
+        const countEl = document.getElementById('copy-counterX');
+        countEl.innerText = parseInt(countEl.innerText) + 1;
+    }
+});
+///////////////////////// [copying numbers]
+document.getElementById('btn-one').addEventListener('click',function(event){
+    const number = document.getElementById('national-emergency-number').innerText;
+    alert ('number copied: '+number);
+    navigator.clipboard.writeText(number);
+})
+document.getElementById('btn-two').addEventListener('click',function(event){
+    const number = document.getElementById('police-help-number').innerText;
+    alert ('number copied: '+number);
+    navigator.clipboard.writeText(number);
+})
+document.getElementById('btn-three').addEventListener('click',function(event){
+    const number = document.getElementById('fire-service-number').innerText;
+    alert ('number copied: '+number);
+    navigator.clipboard.writeText(number);
+})
+document.getElementById('btn-four').addEventListener('click',function(event){
+    const number = document.getElementById('ambulance-service-number').innerText;
+    alert ('number copied: '+number);
+    navigator.clipboard.writeText(number);
+})
+document.getElementById('btn-five').addEventListener('click',function(event){
+    const number = document.getElementById('wAndC-helpline-number').innerText;
+    alert ('number copied: '+number);
+    navigator.clipboard.writeText(number);
+})
+document.getElementById('btn-six').addEventListener('click',function(event){
+    const number = document.getElementById('anti-corruption-number').innerText;
+    alert ('number copied: '+number);
+    navigator.clipboard.writeText(number);
+})
+document.getElementById('btn-seven').addEventListener('click',function(event){
+    const number = document.getElementById('electricity-helpline-number').innerText;
+    alert ('number copied: '+number);
+    navigator.clipboard.writeText(number);
+})
+document.getElementById('btn-eight').addEventListener('click',function(event){
+    const number = document.getElementById('brac-helpline-number').innerText;
+    alert ('number copied: '+number);
+    navigator.clipboard.writeText(number);
+})
+document.getElementById('btn-nine').addEventListener('click',function(event){
+    const number = document.getElementById('bdRailway-helpline-number').innerText;
+    alert ('number copied: '+number);
+    navigator.clipboard.writeText(number);
+})
+
+///////////////////////////////// [Heart Button Functionality]
+document.body.addEventListener('click', e => {
+    if (e.target.classList.contains('fa-heart')) {
+        const countEl = document.getElementById('heart-count');
+        countEl.innerText = parseInt(countEl.innerText) + 1;
+    }
+});
+
+
+
